81d96f0a11fe45c2ef8715d99794d5611f03a0fc
Samuel Farkas <samuel.farkas98@gmail.com>
2018-12-23 16:38:03 +0100

    Added action button

--- app/components/Header/Header.js
+++ app/components/Header/Header.js
@@ -1,6 +1,7 @@
 // @flow
 import React from 'react';
 import { withRouter } from 'react-router-dom';
+import electron from 'electron';
 import styles from './Header.css';
 import NoteButton from '../NoteButton/NoteButton';
 
@@ -24,13 +25,30 @@ class Header extends React.Component<Props> {
     this.props.history.push('/');
   };
 
+
+  closeApp = () => {
+    electron.remote.getCurrentWindow().close();
+  };
+
+  minimizeApp = () => {
+    electron.remote.getCurrentWindow().minimize();
+  };
+
+
   render() {
     const createButton = this.props.location.pathname === '/taskcreator' ? null :
       <NoteButton onClick={this.onCreateNewTask}>+ Create new task</NoteButton>;
     return (
       <div className={styles.header}>
-        <NoteButton padding="20px 73px" onClick={this.props.location.pathname !== '/' ? this.onBrandClick : null}>Note Tracker</NoteButton>
-        {createButton}
+        <div>
+          <NoteButton padding="20px 73px" onClick={this.props.location.pathname !== '/' ? this.onBrandClick : null}>Note
+            Tracker</NoteButton>
+          {createButton}
+        </div>
+        <div className={styles.actionButtons}>
+          <div className={styles.actionButton} onClick={this.minimizeApp}><i className="fas fa-window-minimize"></i></div>
+          <div className={styles.actionButton} onClick={this.closeApp}><i className="fas fa-times"></i></div>
+        </div>
       </div>
     );
   }
